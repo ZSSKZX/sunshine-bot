e8ad3c8a77f6fd7c71ae512f51352b9b27830e1d
ZSSKZX <zsskzx@gmail.com>
2025-04-19 08:47:23 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+API_TOKEN = '7937391885:AAGXnD640_IlrTFzL7Vw1CbW9d_nSHd4oxY'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer("Доброе утро, солнышко❤️ Я теперь всегда с тобой.")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer("Я здесь, рядом, и слушаю тебя…")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
